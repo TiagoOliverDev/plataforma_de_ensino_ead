603d12ae148c2fcb1e9169a4e90cf1fb76e1b5bf
Paull20-20 <paulloholiveira77@gmail.com>
2022-05-27 22:33:30 -0300

    Criação de método para impedir o cadastro de e-mail repetido

--- plataforma_ead/accounts/forms.py
+++ plataforma_ead/accounts/forms.py
@@ -1,14 +1,35 @@
+import email
 from msilib.schema import Class
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm): 
     email = forms.EmailField(label='E-mail')
 
+    #salvando cadastro user
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
+
+    #verificação se e-mail já existe no db
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Já existe usuário com este e-mail')
+        else:
+            return email
+
+
+
+
+
+
+
+
+
+
